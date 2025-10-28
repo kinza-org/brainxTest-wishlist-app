d8a17eb3c7afd3bf9a8ffa8c5dc5ce0e07a486ed
kinza shahzad <kinza.shahzad834@gmail.com>
2025-10-28 16:21:06 +0500

    feat: wishlist functionality on pdp pages, wishlist page and product cards"

--- assets/wishlist.js
+++ assets/wishlist.js
@@ -1,18 +1,22 @@
-// Prevent redefining if script runs multiple times
 if (!customElements.get("wishlist-button")) {
   class WishlistButton extends HTMLElement {
     constructor() {
       super();
       this.apiBaseUrl = "https://brainxtest-wishlist-app-backend.vercel.app";
       this.button = this.querySelector("button");
+      this.spinner = this.querySelector(".loading__spinner");
       this.productId = this.button?.dataset.productId;
       this.customerId = this.button?.dataset.customerId;
       this.isInWishlist = false;
+      this.buttonText = this.button.querySelector("span");
     }
 
     async connectedCallback() {
       // Redirect to login if not logged in
       if (!this.customerId || this.customerId === "") {
+       if (!window.location.pathname.includes('/account/login') && !window.location.pathname.includes('/account/register')) {
+              sessionStorage.setItem('redirectAfterLogin', window.location.href);
+        }
         this.button.addEventListener("click", () => {
           window.location.href = "/account/login";
         });
@@ -21,26 +25,36 @@ if (!customElements.get("wishlist-button")) {
 
       // Load current wishlist and set state
       const wishlist = await this.getWishlist();
+
       this.isInWishlist = wishlist.includes(this.productId);
      // this.updateButton();
 
       // Toggle wishlist state on click
       this.button.addEventListener("click", async () => {
-        if (this.isInWishlist) {
-          await this.removeFromWishlist();
-          this.isInWishlist = false;
-        } else {
-          await this.addToWishlist();
-          this.isInWishlist = true;
+        this.spinner.classList.remove("hidden");
+        this.buttonText.classList.add("hidden");
+        this.spinner.classList.add("loading");
+
+        try {
+          if (this.button.classList.contains("remove-wishlist")) {
+              await this.removeFromWishlist();
+          } else {
+              await this.addToWishlist();
+          }
+        } finally {
+            this.spinner.classList.remove("loading");
+            this.spinner.classList.add("hidden");
+            this.buttonText.classList.remove("hidden");
         }
-      // this.updateButton();
       });
     }
 
     updateButton() {
-      this.button.textContent = this.isInWishlist
+      if (this.buttonText) {
+        this.buttonText.textContent = this.button.classList.contains("remove-wishlist")
         ? "Remove from Wishlist"
         : "Add to Wishlist";
+      }
     }
 
     async getWishlist() {
@@ -64,7 +78,10 @@ if (!customElements.get("wishlist-button")) {
             productId: this.productId,
           }),
         });
+        this.button.classList.remove("add-wishlist");
+        this.button.classList.add("remove-wishlist");
         this.updateButton();
+        this.isInWishlist = true;
       } catch (err) {
         console.error("Error adding to wishlist:", err);
       }
@@ -80,7 +97,10 @@ if (!customElements.get("wishlist-button")) {
             productId: this.productId,
           }),
         });
+        this.button.classList.remove("remove-wishlist");
+        this.button.classList.add("add-wishlist");
         this.updateButton();
+        this.isInWishlist = false;
       } catch (err) {
         console.error("Error removing from wishlist:", err);
       }
